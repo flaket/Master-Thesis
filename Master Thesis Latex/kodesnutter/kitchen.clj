e9d80106cb7cfa7252164d4918fddfe4cfab9185
Andreas A. Flakstad <andflak@gmail.com>
2015-06-10 23:20:36 +0200

    Final touches chapter 0-3 complete

--- Master Thesis Latex/kodesnutter/kitchen.clj
+++ Master Thesis Latex/kodesnutter/kitchen.clj
@@ -1,14 +1,22 @@
 (defn kitchen [z]
-  [:div {:className (str "room" z (when (:lights-off? (:livingroom (:rooms @state))) " dark"))}
+  [:div {:className (str "room" z (when (:lights-off?
+                                    (:livingroom
+                                        (:rooms @state)))
+                                            " dark"))}
    [:img.room-image {:src       (:kitchen imgs)
-                     :on-click #(swap! state assoc :view :kitchen)}]
+                     :on-click #(swap! state assoc
+                                :view :kitchen)}]
    [:img.fridge {:src       (:fridge imgs)
-                 :on-click  #(swap! state assoc :view :food)}]
-   (if (:active? (:dish-washer (:kitchen (:rooms @state))))
+                 :on-click  #(swap! state assoc
+                             :view :food)}]
+   (if (:active? (:dish-washer
+                    (:kitchen (:rooms @state))))
      [:div
       [:img.dish {:src      (:dish-on imgs)
-                  :on-click #(swap! state assoc-in [:rooms :kitchen :dish-washer :active?] false)}]]
+                  :on-click #(swap! state assoc-in
+            [:rooms :kitchen :dish-washer :active?] false)}]]
       [:img.dish {:src      (:dish-off imgs)
-                  :on-click #(swap! state assoc-in [:rooms :kitchen :dish-washer :active?] true)}])
+                  :on-click #(swap! state assoc-in
+            [:rooms :kitchen :dish-washer :active?] true)}])
     ..
     ])
